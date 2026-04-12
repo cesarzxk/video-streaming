49090fc5b64aba978b7dbfc23559a3b57d974141
Cesar <cesar3d.vargas@gmail.com>
2026-04-12 19:07:42 -0300

    fix: correct typo in error message for video conversion

---
+++ src/main/java/com/cesarzxk/initial/videoStreaming/dto/StorageDTO.java
@@ -0,0 +1,9 @@
+package com.cesarzxk.initial.videoStreaming.dto;
+
+import java.util.Map;
+
+public class StorageDTO {
+    private String message;
+    private Map<String, String> files;
+    private Map<String, String> urls;
+}
